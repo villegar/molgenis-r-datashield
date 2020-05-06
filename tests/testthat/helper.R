1db6e678de80cadaddca366849377964417acdba
Fleur Kelpin <f.kelpin@umcg.nl>
2020-05-06 21:40:34 +0200

    test: test all the things

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -6,3 +6,11 @@ cookies <- tibble(
   name = "JSESSIONID",
   value = "abcde"
 )
+handle <- httr::handle(url = "http://example.org:8080")
+connection <- methods::new("ArmadilloConnection",
+  name = "name",
+  handle = handle,
+  workspaces = list("GECKO/customer"),
+  user = "admin",
+  cookies = cookies
+)
