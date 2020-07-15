5d4af7319eefc091ff66313efd6e0cf62bfd3859
sidohaakma <sido@haakma.org>
2020-07-15 17:46:42 +0200

    tests: fixed test

--- tests/testthat/test-ArmadilloOAuth.R
+++ tests/testthat/test-ArmadilloOAuth.R
@@ -1,17 +1,14 @@
-library(httr)
-
 test_that("get_token test if the token information is retrieved correctly", {
-  authInfo <- list(issuerUri = "https://auth.server.org", clientId = "xxxxx-xxxxx")
-  get <- mock(list(status_code = status_code, auth = authInfo))
-  discover <- mock(list(endpoint = "https://auth.server.org/discover"))
-  deviceFlowAuth <- mock("abcd-abcd")
-  statusCode = mock(200)
+  content <- mock()
+  discover <- mock()
+  get <- mock(structure(list(status_code = 200), class = "response"))
+  credentials <- mock(list(id_token = "abcd-abcd"))
   
   with_mock(
     "httr::GET" = get,
-    "httr::content" = get,
+    "httr::content" = content,
     "MolgenisAuth::discover" = discover,
-    #"MolgenisAuth::device_flow_auth" = deviceFlowAuth,
+    "MolgenisAuth::device_flow_auth" = credentials,
     token <- datashield.get_token("https://armadillo.dev.molgenis.org")
   )
   expect_equal(token, "abcd-abcd")
