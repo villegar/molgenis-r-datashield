0c453c28da98e4236d12d4c55926ada72827ede5
sidohaakma <sido@haakma.org>
2020-08-28 15:27:28 +0200

    chore: added pictures

--- vignettes/pre-render.R
+++ vignettes/pre-render.R
@@ -1,2 +1,6 @@
-knitr::knit("vignettes/articles/DSMolgenisArmadillo.Rmd.orig", output = "vignettes/articles/DSMolgenisArmadillo.Rmd")
-knitr::knit("vignettes/articles/development.Rmd.orig", output = "vignettes/articles/development.Rmd")
+library(knitr)
+# please note that rendered images are rendered in the project-root
+# you need to copy those to the vignettes directory
+# you can not use 'fig.path' because the path is used in the <img> tag which then refers to the relative path you type in
+knitr::knit("vignettes/DSMolgenisArmadillo.Rmd.orig", output = "vignettes/DSMolgenisArmadillo.Rmd")
+knitr::knit("vignettes/development.Rmd.orig", output = "vignettes/development.Rmd")
