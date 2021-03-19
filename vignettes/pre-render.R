9f5a3bc628bec1bd2cb8e449aceb7baec1c2b826
Fleur Kelpin <f.kelpin@umcg.nl>
2021-03-19 21:59:37 +0100

    style: fix line endings in vignette pre-render

--- vignettes/pre-render.R
+++ vignettes/pre-render.R
@@ -1,7 +1,15 @@
 library(knitr)
 # please note that rendered images are rendered in the project-root
 # you need to copy those to the vignettes directory
-# you can not use 'fig.path' because the path is used in the <img> tag which then refers to the relative path you type in
-knitr::knit("vignettes/DSMolgenisArmadillo.Rmd.orig", output = "vignettes/DSMolgenisArmadillo.Rmd")
-knitr::knit("vignettes/development.Rmd.orig", output = "vignettes/development.Rmd")
-knitr::knit("vignettes/workspaces.Rmd.orig", output = "vignettes/workspaces.Rmd")
+# you can not use 'fig.path' because the path is used in the <img> tag which
+# then refers to the relative path you type in
+knitr::knit("vignettes/DSMolgenisArmadillo.Rmd.orig",
+  output = "vignettes/DSMolgenisArmadillo.Rmd"
+)
+knitr::knit("vignettes/development.Rmd.orig",
+  output = "vignettes/development.Rmd"
+)
+knitr::knit("vignettes/workspaces.Rmd.orig",
+  output = "vignettes/workspaces.Rmd"
+)
+
